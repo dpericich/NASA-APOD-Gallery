64a8f4c11e0bbb91ff1b3843ec2751fbba8110b9
Daniel <dpericich@gmail.com>
2020-12-12 14:23:53 -0600

    clearing out current branch

--- src/components/selected-image/SelectedImage.js
+++ src/components/selected-image/SelectedImage.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 const SelectedImage = (props) => {
-    return(
-        <img src={props.imgData.url} alt={props.imgData.title} className="selected-image"/>
-    )
+    if (props.loading) {
+        return <h2>Loading...</h2>;
+    } else {
+        return(
+            <img src={props.imgData.url} alt={props.imgData.title} className="selected-image"/>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
-    return {imgData: state.img.spaceImg}
+    return {imgData: state.img.spaceImg, loading: state.loading}
 }
 
 export default connect(mapStateToProps, null)(SelectedImage);
